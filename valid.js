2c5c94b45a7ffbf18db55b87077017a7d9cbbe40
AdiWijaya2 <97686928+AdiWijaya2@users.noreply.github.com>
2022-01-15 09:40:28 +0800

    Add files via upload

---
+++ valid.js
@@ -0,0 +1,22 @@
+
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Adi Wijaya" && password == "nayotama1234"){
+alert ("Login successfully");
+window.location.href = "Dashboard.html"; // Menuju halaman berikutnya
+return false;
+}
+else if (username == '' && password == '' ){
+    alert("Masukkan Username dan Password");
+}
+else if (username != username && password == password ){
+    alert("Username anda salah, coba lagi");
+}
+else if (username == username && password != password ){
+    alert("Password anda salah, coba lagi");
+}
+else{
+    alert("Username dan password anda salah");
+}
+}
